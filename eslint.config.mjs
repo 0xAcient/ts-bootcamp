17a97f533f6a0f45271f60f5885b1a38abecd9ea
0xAcient <acientguru@gmail.com>
2026-01-07 00:03:51 +0100

    fixed issues with husky and eslint no-explicit-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ export default [
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off",
+     "@typescript-eslint/no-explicit-any": "error"
     },
   },
 ];
--- src/discriminated.ts
+++ src/discriminated.ts
@@ -1,16 +1,16 @@
-type TextField = {kind: "text"; maxLength: number};
-type NumberField = {kind: "number"; min: any}
+type TextField = { kind: 'text'; maxLength: number };
+type NumberField = { kind: 'number'; min: number };
 
 type Field = TextField | NumberField;
 
 export function describeField(field: Field): string {
+  switch (field.kind) {
+    case 'text':
+      return 'This represent the text field';
+    case 'number':
+      return 'This represent the number field';
 
-    switch(field.kind) {
-        case "text": 
-            return "This represent the text field";
-        case "number":
-            return "This represent the number field";
-    }
-
-    return "Something went wrong";
+    default:
+      return 'Something went wrong';
+  }
 }
